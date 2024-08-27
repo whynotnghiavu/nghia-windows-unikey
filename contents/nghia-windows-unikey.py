a12792e905e86fcefc03b5f5e6c9361fe0235620
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-08-27 09:47:13 +0700

    copy 1 file and update 2 files

--- contents/nghia-windows-unikey.py
+++ contents/nghia-windows-unikey.py
@@ -1,52 +1,35 @@
 import os
-import subprocess
-import sys
-import ctypes
 
 
-def is_admin():
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
 
 
-if is_admin():
 
-    Downloads = os.path.expanduser(r"~\Downloads")
-    print(f"🚀 {Downloads}")
 
-    # Đường dẫn đến thư mục VietNam
-    VietNam = os.path.join(Downloads, r"Nghia\Programs\VietNam")
-    print(f"🚀 {VietNam}")
 
-    keymap_txt_link = os.path.join(VietNam, "keymap.txt")
-    print(f"🚀 {keymap_txt_link}")
 
-    # Đường dẫn đến file keymap.txt
-    keymap_txt = os.path.join(os.getcwd(), "keymap.txt")
-    print(f"🚀 {keymap_txt}")
 
-    # Xóa liên kết
+
+Downloads = os.path.expanduser(r"~\Downloads")
+print(f"🚀 {Downloads}")
+
+
+VietNam = os.path.join(Downloads, r"Nghia\Programs\VietNam")
+print(f"🚀 {VietNam}")
+
+keymap_txt_link = os.path.join(VietNam, "keymap.txt")
+print(f"🚀 {keymap_txt_link}")
+
+
+nghia_windows_unikey_txt = os.path.join(os.getcwd(), "nghia-windows-unikey.txt")
+print(f"🚀 {nghia_windows_unikey_txt}")
+
+
+
+try:
     if os.path.exists(keymap_txt_link):
         os.remove(keymap_txt_link)
-        print(f"Đã xóa file: {keymap_txt_link}")
-    else:
-        print(f"File không tồn tại: {keymap_txt_link}")
-
-    # Tạo liên kết
-    try:
-        # subprocess.run(['mklink', keymap_txt_link, keymap_txt], shell=True, check=True)
-        subprocess.run(['mklink', keymap_txt_link, keymap_txt], shell=True, check=True)
-        print(f"Tạo liên kết thành công: {keymap_txt_link}")
-    except subprocess.CalledProcessError as e:
-        print(f"Lỗi khi tạo liên kết: {e}")
-    except FileExistsError:
-        print("Liên kết đã tồn tại")
-    except OSError as e:
-        print(f"Lỗi hệ thống khi tạo liên kết: {e}")
-
-
-else:
-    ctypes.windll.shell32.ShellExecuteW(
-        None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+        print(f"Đã xóa symlink: {keymap_txt_link}")
+    os.symlink(os.path.abspath(nghia_windows_unikey_txt), keymap_txt_link)
+    print(f"Tạo link: {keymap_txt_link}")
+except OSError as e:
+    print(f"Lỗi: {e}")
